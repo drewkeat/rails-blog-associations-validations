ad64f2cf4691730dae0c92ec57bd247c1b4e2244
Drew Keat <thedrewkeat@gmail.com>
2021-08-17 08:23:02 -0600

    establish validations, and begin relationships

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :name
       t.text :content 
+      t.belongs_to :user
+
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210817140545) do
+ActiveRecord::Schema.define(version: 20210817142151) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
